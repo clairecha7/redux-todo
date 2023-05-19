7655963bcb7a080d4627576432283bf8a9587549
clairecha7 <mybarclaire@gmail.com>
2023-05-18 22:55:35 -0700

    first commit

---
+++ src/redux/actions/actions.jsx
@@ -0,0 +1,33 @@
+import {
+  ADD_TODO,
+  CLEAR_TODO_LIST,
+  DELETE_TODO,
+  TOGGLE_TODO,
+} from "../types/types";
+
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload,
+  };
+};
+
+export const clearTodoList = () => {
+  return {
+    type: CLEAR_TODO_LIST,
+  };
+};
+
+export const toggleTodo = (payload) => {
+  return {
+    type: TOGGLE_TODO,
+    payload,
+  };
+};
+
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload,
+  };
+};
